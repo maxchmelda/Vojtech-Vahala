f323cdc8a138d72cf6a8b8d7971c5eef61693bf1
Max Chmelicek <max.chmelicek@gmail.com>
2026-02-01 21:11:52 +0100

    added hidden handwriting

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='bg-[#00171F] py-4 sm:py-8 text-center text-white font-lexend'>
-        &copy; {new Date().getFullYear()} Vojtěch Vahala
+    <footer className='bg-[#00171F] py-4 sm:py-8 text-center text-white font-lexend relative'>
+      <p>&copy; {new Date().getFullYear()} Vojtěch Vahala</p>
+      <p className='aboslute text-transparent text-xs'>Made by Max Chmelicek and Adam Hladik</p>
     </footer>
   )
 }
