942256410dfaf158933d91ad73e47dde69c777bf
Max Chmelicek <max.chmelicek@gmail.com>
2026-02-01 21:59:45 +0100

    handwriting fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='bg-[#00171F] py-4 sm:py-8 text-center text-white font-lexend relative'>
       <p>&copy; {new Date().getFullYear()} Vojtěch Vahala</p>
-      <p className='aboslute text-transparent text-xs'>Made by Max Chmelicek and Adam Hladik</p>
+      <p className='absolute text-transparent text-xs bottom-0 left-1/2 -translate-x-1/2'>Made by Max Chmelicek and Adam Hladik</p>
     </footer>
   )
 }
